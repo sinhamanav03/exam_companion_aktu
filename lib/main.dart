34a7974192d51f96181f984a24c02fc2d0851df5
Manav Sinha <manavsinha03@gmail.com>
2020-05-13 12:04:22 +0530

    Homepage Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,139 +10,388 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Exam Companion',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Aktu companion'),
+      home: MyHomePage(title: 'Exam companion'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(16.0),
-              leading: Icon(Icons.assignment),
-              title: Text(
-                "Notes Section",
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => NotesPage(),
+          Expanded(
+            flex: 2,
+            child: ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+                //height: 350,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xFF3383CD),
+                      Color(0xFF1124),
+                    ],
                   ),
-                );
-                // setState(() {
-                //   buildNotesPage();
-                // });
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(16.0),
-              leading: Icon(Icons.account_balance),
-              title: Text(
-                "Previous Year Question",
-                style: TextStyle(
-                  fontSize: 24.0,
+                  //image: DecorationImage(image: AssetImage('assets/images/bg1.jpg'))
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: SvgPicture.asset(
+                        'assets/icons/menu.svg',
+                        color: Colors.black87,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Expanded(
+                      child: Stack(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            'assets/icons/books.svg',
+                            width: 150,
+                            fit: BoxFit.fitWidth,
+                            alignment: Alignment.topCenter,
+                          ),
+                          Positioned(
+                            top: 20,
+                            left: 170,
+                            child: Text(
+                              "      Exam \n Companion",
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black87,
+                                fontFamily: 'Lobster',
+                              ),
+                            ),
+                          ),
+                          Container(),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(16.0),
-              leading: Icon(Icons.assignment),
-              title: Text(
-                "Notice",
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.0,
+              ),
+              child: Row(
+                children: <Widget>[
+                  buildExpandedNode('notes2', 'Notes','',context),
+                  SizedBox(width: 20),
+                  buildExpandedNode('notes', 'Previous year Papers','',context),
+                ],
               ),
             ),
           ),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          )
         ],
       ),
     );
   }
 
-  NotesPage buildNotesPage() => new NotesPage();
+  Expanded buildExpandedNode(String icon, String title,String page,BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: <Widget>[
+          FlatButton(
+            child: Container(
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25.0),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 33,
+                    offset: Offset(0, 10),
+                    color: Color(0xFFD3D3D3).withOpacity(.84),
+                  ),
+                ],
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,MaterialPageRoute(builder: (context) =>NotesPage())
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: SvgPicture.asset(
+              'assets/icons/$icon.svg',
+              height: 100,
+            ),
+          ),
+          Positioned(
+              bottom: 70,
+              left: 20,
+              child: Text(
+                title,
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro', fontWeight: FontWeight.bold),
+              ))
+        ],
+      ),
+    );
+  }
 }
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//   final String title;
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Row(
+//         children: <Widget>[
+//           buildContainer(),
+//           buildContainer(),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget buildContainer() {
+//     return Expanded(
+//       flex: 1,
+//       child: Container(
+//         padding: EdgeInsets.all(8.0),
+//         margin: EdgeInsets.all(12.0),
+//         child: Center(
+//           child: RichText(
+//             text: TextSpan(
+//                 text: '123',
+//                 style: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.black,
+//                   fontSize: 20.0,
+//                 )),
+//           ),
+//         ),
+//         decoration: BoxDecoration(
+//           color: Colors.white,
+//           borderRadius: BorderRadius.circular(10.0),
+//           boxShadow: <BoxShadow>[
+//             BoxShadow(
+//               color: Colors.black,
+//               blurRadius: 50.0,
+//               spreadRadius: 2.0,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   NotesPage buildNotesPage() => new NotesPage();
+// }
 
 class NotesPage extends StatefulWidget {
   @override
   _NotesPageState createState() => _NotesPageState();
 }
 
-final List<String> SubjectList = [
-  '---Select Subject---',
+final List<String> subjectList = [
+  '-------Select Subject-------',
   'Electronics Engineering',
   'Theroy of automata and languages',
+  'Microprocessor',
+  'Technical Communication'
 ];
-String dropDownValue = SubjectList[0];
+String dropDownValue = subjectList[0];
 
 class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
+        backgroundColor: Colors.black54,
         appBar: AppBar(
-          title: Text("Notes Section"),
+          title: Text(
+            "Notes Section",
+            textAlign: TextAlign.justify,
+          ),
         ),
-        
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: DropdownButton<String>(focusColor: Color(0xFFEF9A9A),
-          iconSize: 60.0,
-              items: SubjectList.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  child: Text(value),
-                  value: value,
-                );
-              }).toList(),
-               value: dropDownValue,
-               style: TextStyle(color: Colors.red),
-              onChanged: (String newValue){setState(() {
-                 dropDownValue = newValue;
-              });}),
-        ),
+        body: bulitNotesPageBody(),
       ),
     );
   }
+
+  Widget bulitNotesPageBody() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        buildDropdown(),
+        buildListTile("Unit - 1"),
+        buildListTile("Unit - 2"),
+        buildListTile("Unit - 3"),
+        buildListTile("Unit - 4"),
+        buildListTile("Unit - 5"),
+      ],
+    );
+  }
+
+  Padding buildListTile(String text) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListTile(
+        leading: Icon(
+          Icons.assignment,
+          size: 30.0,
+          color: Colors.white70,
+        ),
+
+        // contentPadding: EdgeInsets.all(20.0),
+        title: Text(
+          text,
+          style: TextStyle(fontSize: 30.0, color: Colors.white70),
+        ),
+        onTap: () {
+          setState(() {
+            _launchURL();
+          });
+        },
+      ),
+    );
+  }
+
+  _launchURL() async {
+    const url = 'https://flutter.dev';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Padding buildDropdown() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(40, 30, 20, 20),
+      child: DropdownButton<String>(
+          autofocus: true,
+          elevation: 10,
+          focusColor: Colors.amber,
+          dropdownColor: Colors.black87,
+          iconSize: 40.0,
+          items: subjectList.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              child: Text(value),
+              value: value,
+            );
+          }).toList(),
+          value: dropDownValue,
+          style: TextStyle(color: Colors.white70),
+          onChanged: (String newValue) {
+            setState(() {
+              dropDownValue = newValue;
+            });
+          }),
+    );
+  }
 }
+
+// Column(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: <Widget>[
+//             Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: ListTile(
+//                 contentPadding: EdgeInsets.all(16.0),
+//                 leading: Icon(Icons.assignment),
+//                 title: Text(
+//                   "Notes Section",
+//                   style: TextStyle(
+//                     fontSize: 24.0,
+//                   ),
+//                 ),
+//                 onTap: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                       builder: (context) => NotesPage(),
+//                     ),
+//                   );
+//                 },
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: ListTile(
+//                 contentPadding: EdgeInsets.all(16.0),
+//                 leading: Icon(Icons.account_balance),
+//                 title: Text(
+//                   "Previous Year Question",
+//                   style: TextStyle(
+//                     fontSize: 24.0,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: ListTile(
+//                 contentPadding: EdgeInsets.all(16.0),
+//                 leading: Icon(Icons.assignment),
+//                 title: Text(
+//                   "Notice",
+//                   style: TextStyle(
+//                     fontSize: 24.0,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
